00ac073ac4b82f6f2f29b02f62a104e07a048328
Illidanz <illidanrulez@gmail.com>
2019-12-29 13:10:50 +0100

    Fixed linebreak bug

--- repack_cnut.py
+++ repack_cnut.py
@@ -76,7 +76,7 @@ def run():
                                     newsjis = pre + newsjis + post
                                     if newsjis != check:
                                         newsjis = common.wordwrap(newsjis, glyphs, 205, game.detectTextCode)
-                                        newsjis = newsjis.replace("|", "<0A>").replace(">>", "#INP()" + pre)
+                                        newsjis = newsjis.replace(">>", "#INP()" + pre).replace("|", "<0A>")
                                         # Copy data up to here
                                         endpos = fin.tell()
                                         fin.seek(lastpos)
